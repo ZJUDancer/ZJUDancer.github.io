04713dae639cb650bb9e989899a6e15c800aaf76
waterda <3364606254@qq.com>
2026-04-14 12:02:28 +0800

    修git

--- assets/js/doc-config.js
+++ assets/js/doc-config.js
@@ -123,7 +123,7 @@ const TUTORIALS_NAV = [
       }, 
       {
         title: "4.git基础知识",
-        file: "docs/tutorials/git基础知识/git基础知识.md",
+        file: "docs/tutorials/git基础知识/git基础知识/Git基本知识.md",
         docId: "git"
       },
       {
